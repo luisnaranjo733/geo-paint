526b4afc7bf0a208b3408bc3bf8e05a846cb8112
Luis Naranjo <luisnaranjo733@gmail.com>
2016-02-07 21:51:22 -0800

    added gitignore

--- GeoPaint/app/src/main/java/edu/uw/jnaranj0/geopaint/MapsActivity.java
+++ GeoPaint/app/src/main/java/edu/uw/jnaranj0/geopaint/MapsActivity.java
@@ -23,12 +23,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "onCreate!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
 
     }
 
