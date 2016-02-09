9339e142569690586d8c71ede4da9a3d522eacb9
Luis Naranjo <luisnaranjo733@gmail.com>
2016-02-08 21:45:32 -0800

    permissions working

--- GeoPaint/app/src/main/java/edu/uw/jnaranj0/geopaint/MapsActivity.java
+++ GeoPaint/app/src/main/java/edu/uw/jnaranj0/geopaint/MapsActivity.java
@@ -1,9 +1,14 @@
 package edu.uw.jnaranj0.geopaint;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.ActionMenuItemView;
 import android.util.Log;
@@ -11,7 +16,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,8 +28,69 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.Manifest;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    // Start of GoogleApiClient.ConnectionCallbacks methods
+    private final int PERMISSION_REQUEST_CODE = 19;
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.v(TAG, "On connected");
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        LocationRequest request = new LocationRequest();
+        request.setInterval(10000);
+        request.setFastestInterval(5000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            Log.v(TAG, "Location permission granted!");
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, request, this);
+
+        } else {
+            Log.v(TAG, "Location permission NOT granted :(");
+            String[] permissions = new String[1];
+            permissions[0] = Manifest.permission.ACCESS_FINE_LOCATION;
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        Log.v(TAG, "Permission request result: " + grantResults.toString());
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.v(TAG, "On connection suspended: " + i);
+    }
+
+    // End of GoogleApiClient.ConnectionCallbacks methods
 
     public static final String TAG = "GeoPaint";
 
@@ -40,14 +109,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-//        if (mGoogleApiClient == null) {
-//            mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                    .addConnectionCallbacks(this)
-//                    .addOnConnectionFailedListener(this)
-//                    .addApi(LocationServices.API)
-//                    .build();
-//        }
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            Log.v(TAG,  "Instantiated google api client");
+        }
 
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
     }
 
 
@@ -104,5 +183,17 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     public void shareImage(MenuItem item) {
         Log.v(TAG, "Share the image!");
+    }
+
+    // GoogleApiClient.OnConnectionFailedListener callback
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, connectionResult.toString());
+    }
+
+    // LocationListener method
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v(TAG, "Location changed: " +   location.toString());
     }
 }
